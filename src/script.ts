90f918c4c165c32f435ab921abea0b4bc2bbba4e
sagormohammedyousuf <smyousuf7999@gmail.com>
2025-02-10 19:26:00 +0600

    practice

--- src/script.ts
+++ src/script.ts
@@ -264,3 +264,6 @@ const userRole1: userRole = userRole.admin;
 function identity <T> (value: T) : T {
   return value;
 }
+
+console.log(identity<string>("Hello"));
+console.log(identity<number>(42));
